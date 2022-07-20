da79ae13b9be0a107b213b0722e395cb2ebad75f
liubin_GSP <liubin@qq.com>
2022-07-21 07:17:59 +0800

    p64 第三方签名服务的配置和网关设置

---
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/gulimallthirdparty/controller/OssController.java
@@ -0,0 +1,96 @@
+package com.atguigu.gulimall.gulimallthirdparty.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * ClassName: Control.
+ * Description:
+ * date: 2022/7/21 6:36
+ *
+ * @author Control.
+ * @since JDK 1.8
+ */
+@RestController
+public class OssController {
+
+    @Resource
+    OSS client;
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    private String endpoint;
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    String bucket;
+    @Value("${spring.cloud.alicloud.accesskey}")
+    String accessId;
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy(){
+        Map<String, String> respMap=null;
+//        https://gulimail-hellolb.oss-cn-shanghai.aliyuncs.com/001.jpg
+        // 填写Bucket名称，例如examplebucket。
+
+        // 填写Host地址，格式为https://bucketname.endpoint。
+        String host = "https://"+bucket+endpoint;
+        // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
+//        String callbackUrl = "https://192.168.0.0:8888";
+        // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format+"";
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = client.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = client.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+
+            // respMap.put("expire", formatISO8601Date(expiration));
+//            文件上传 的跨域请求处理
+//            JSONObject jasonCallback = new JSONObject();
+//            jasonCallback.put("callbackUrl", callbackUrl);
+//            jasonCallback.put("callbackBody",
+//                    "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}");
+//            jasonCallback.put("callbackBodyType", "application/x-www-form-urlencoded");
+//            String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
+//            respMap.put("callback", base64CallbackBody);
+//
+//            JSONObject ja1 = JSONObject.fromObject(respMap);
+//            // System.out.println(ja1.toString());
+//            response.setHeader("Access-Control-Allow-Origin", "*");
+//            response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+//            response(request, response, ja1.toString());
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+        return respMap;
+    }
+
+}
